7f33ce2dc811bfa8490e196a1db7384058a28937
Ngoc Tram <ntram2k3@gmail.com>
2023-11-10 20:36:12 +0700

    feat: add file main

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Ngoc Tram
+ * @project SlangWord
+ * @created 10/11/2023 - 12:42
+ */
+import Controllers.DictionaryController;
+public class Main {
+    public static void main(String[] args) {
+        DictionaryController program = new DictionaryController();
+    }
+}
